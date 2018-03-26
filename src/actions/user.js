8f4c711a7f3a6f765c897e7791d0cc16a05847c1
MannyC <github-qqq@snkmail.com>
2018-03-26 15:43:29 +0100

    Reload securities on mount if in failed state.  Remove failure message on successful update.

--- src/actions/user.js
+++ src/actions/user.js
@@ -3,9 +3,14 @@ import { fetchSecurities } from './securities'
 export const SET_BASE_CURRENCY = 'SET_BASE_CURRENCY'
 
 export const setBaseCurrency = currency => (dispatch, getState) => {
+  const originalCurrency = getState().user.baseCurrency
+
   dispatch({
     type: SET_BASE_CURRENCY,
     currency
   })
-  fetchSecurities()(dispatch, getState)
+
+  if (currency !== originalCurrency) {
+    fetchSecurities()(dispatch, getState)
+  }
 }
