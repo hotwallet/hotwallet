ce605d79ffc5bbb5b92be63860b261cd540f1194
will123195 <will123195@gmail.com>
2018-08-29 16:08:38 -0600

    try to get add to homescreen working on android

--- src/lib/addToHomeScreen.js
+++ src/lib/addToHomeScreen.js
@@ -1,22 +1,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js')
-    .then(reg => {
-      console.log('service-worker.js registered:', reg)
-    })
-    .catch(err => {
-      console.log(err)
-    })
 }
 
 window.addEventListener('beforeinstallprompt', e => {
-  console.log('beforeinstallprompt', e)
   e.preventDefault()
   e.prompt()
-  e.userChoice.then(result => {
-    if (result.outcome === 'dismissed') {
-      console.log('install prompt dismissed')
-      return
-    }
-    console.log('install prompt accepted')
-  })
 })
