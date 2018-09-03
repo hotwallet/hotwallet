032a0364a5c53e2efad75ff750095d90b4b97d6d
will123195 <will123195@gmail.com>
2018-09-03 12:57:13 -0600

    ledger connect coming soon message

--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import H1 from './H1'
+import { mobilePadding, desktopPadding } from '../lib/styles'
 
-class Settings extends React.Component {
+class Ledger extends React.Component {
   render() {
     return (
       <div>
         <H1 text="Ledger Connect" />
+        <div
+          style={{
+            padding: this.props.isMobile ? mobilePadding : desktopPadding
+          }}
+        >
+          Coming soon.
+        </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  isMobile: state.app.isMobile
+})
 
-export default connect(mapStateToProps)(Settings)
+export default connect(mapStateToProps)(Ledger)
