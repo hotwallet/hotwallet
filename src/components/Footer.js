9fe0e45579923f496c19efe3fb5753bc2d77019f
will123195 <will123195@gmail.com>
2018-09-17 23:58:54 -0600

    add github footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Icon } from 'semantic-ui-react'
 import { border, smallFontSize, mobilePadding, desktopPadding } from '../lib/styles'
 
 class Footer extends React.Component {
@@ -15,6 +16,14 @@ class Footer extends React.Component {
     return (
       <footer style={footerStyle}>
         &copy; 2018 Gadget Labs
+        <span style={{ margin: '0 15px' }}>|</span>
+        <a href="https://github.com/hotwallet" style={{ color: '#fff' }}>
+          <Icon
+            name="github"
+            inverted
+          />
+          HotWallet
+        </a>
       </footer>
     )
   }
