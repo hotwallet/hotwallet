e4d6b588142dd2dc4cba772cee0fdeb0c91f7fff
will123195 <will123195@gmail.com>
2018-09-23 08:47:40 -0600

    close mobile menu after navigating

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -37,6 +37,7 @@ class NavLink extends React.Component {
           <Link style={style}
             to={props.to}
             onClick={() => {
+              if (props.onClick) props.onClick()
               this.setState({ showTooltip: false })
             }}
             onMouseOver={() => this.setState({ hover: true, showTooltip: true })}
